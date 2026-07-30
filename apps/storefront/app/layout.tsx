d8dad297fde0e750197d9326c61403931f5edc72
fernndes <fernand3s.dev@gmail.com>
2026-07-30 20:07:00 -0300

    chore: add vercel speed insights

--- apps/storefront/app/layout.tsx
+++ apps/storefront/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import "./globals.css";
 
 import { Header } from "@/components/header/header";
@@ -44,6 +45,7 @@ export default function RootLayout({
         <div id="conteudo" className="flex flex-1 flex-col">
           {children}
         </div>
+        <SpeedInsights />
       </body>
     </html>
   );
